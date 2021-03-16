9c8bf38478d7639df52824d68d2fadaff9cdc4ea
JhsK <qwe6293@nate.com>
2021-03-16 11:49:26 +0900

    최대 값 구하기

---
+++ basic/max.js
@@ -0,0 +1,22 @@
+const a = 4;
+const b = 2;
+const c = 1;
+const d = 3;
+
+function MaxNumber(num1, num2, num3, num4) {
+  let max = num1;
+
+  if (b > max) {
+    max = b;
+  }
+  if (c > max) {
+    max = c;
+  }
+  if (d > max) {
+    max = d;
+  }
+
+  return max;
+}
+
+console.log(MaxNumber(a, b, c, d));
