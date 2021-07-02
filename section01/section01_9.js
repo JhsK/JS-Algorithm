5c0f5de9755e29f57c48d475b17e2e785da253e8
JhsK <qwe6293@nate.com>
2021-07-02 13:52:05 +0900

    A를 #으로 - 문자열, 배열 객체의 복사 차이점

--- section01/section01_9.js
+++ section01/section01_9.js
@@ -1,13 +1,18 @@
 function solution(s) {
-  let answer = "";
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "A") {
-      s = s.replace("A", "#");
-      answer += s[i];
-    } else answer += s[i];
-  }
+  let answer = s;
+  answer = answer.replace(/A/g, "#");
   return answer;
 }
 
 let str = "BANANA";
 console.log(solution(str));
+
+// let i = s; -> 다음과 같은 구문에서 s가 배열이라면 얕은 복사를 통해 서로 주소를 가리키지만
+// s가 문자열이라면 얕은 복사가 아닌 값이 복사가 되어서 서로 다른 것을 가리킨다.
+
+// for (let i = 0; i < s.length; i++) {
+//   if (s[i] === "A") {
+//     s = s.replace("A", "#");
+//     answer += s[i];
+//   } else answer += s[i];
+// }
