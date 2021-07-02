57f6f0ba8e79a8f13a5cb08d29538b67b64ce055
JhsK <qwe6293@nate.com>
2021-07-02 14:03:15 +0900

    문자 찾기

---
+++ section01/section01_10.js
@@ -0,0 +1,11 @@
+function solution(s, t) {
+  let answer = s.split(t).length;
+  return answer - 1;
+}
+
+let str = "COMPUTERPROGRAMMING";
+console.log(solution(str, "R"));
+
+// for (let x of s) {
+//     if (x === t) answer++;
+//   }
