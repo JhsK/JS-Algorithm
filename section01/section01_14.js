907f16c07bbd83c1ec65be2e1037319c68774741
JhsK <qwe6293@nate.com>
2021-07-06 13:38:02 +0900

    가장 긴 문자열

---
+++ section01/section01_14.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  let answer = "";
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].length > max) {
+      max = s[i].length;
+      answer = s[i];
+    }
+  }
+  return answer;
+}
+let str = ["teacher", "time", "student", "beautiful", "good"];
+console.log(solution(str));
