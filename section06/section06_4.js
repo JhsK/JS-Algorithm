26117e2e0eea740d009c77b8f0d94da57a676de7
JhsK <qwe6293@nate.com>
2021-08-05 13:26:08 +0900

    후위식 연산(스택)

--- section06/section06_4.js
+++ section06/section06_4.js
@@ -1,22 +1,21 @@
-function solution(s){  
-    let answer;
-    let stack=[];
-    
-    for (let x of s) {
-        if (!isNaN(x)) stack.push(Number(x));
-        else {
-            let rt = stack.pop();
-            let lt = stack.pop();
-            if (x === '+') stack.push(lt + rt);
-            else if (x === '*') stack.push(lt * rt);
-            else if (x === '-') stack.push(lt - rt);
-            else if (x === '/') stack.push(lt / rt);
-        }
-    }
+function solution(s) {
+  let answer;
+  let stack = [];
 
-    answer = stack[0];
-    return answer;
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") stack.push(lt + rt);
+      else if (x === "*") stack.push(lt * rt);
+      else if (x === "-") stack.push(lt - rt);
+      else if (x === "/") stack.push(lt / rt);
+    }
+  }
+  answer = stack[0];
+  return answer;
 }
 
-let str="352+*9-";
+let str = "352+*9-";
 console.log(solution(str));
