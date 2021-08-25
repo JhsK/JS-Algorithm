ad2e3d88645f5a760a4398dceb9e7babfd683867
JhsK <qwe6293@nate.com>
2021-08-25 14:18:44 +0900

    부분집합 구하기(DFS)

--- section08/section08_04.js
+++ section08/section08_04.js
@@ -1,19 +1,19 @@
 function solution(n) {
     let answer = [];
-    let check = Array.from({length: n+1}, () => 0);
+    let checks = Array.from({length: n+1}, () => 0);
 
     function DFS(L) {
         if (L === n+1) {
             let tmp = "";
             for (let i = 1; i <= n; i++) {
-                if (check[i] === 1) tmp += (i +" ");
+                if (checks[i] === 1) tmp += (i +" ");
             }
             if (tmp.length > 0) answer.push(tmp);
         }
         else {
-            check[L] = 1;
+            checks[L] = 1;
             DFS(L+1);
-            check[L] = 0;
+            checks[L] = 0;
             DFS(L+1);
         }
     }
