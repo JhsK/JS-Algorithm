0c1b3dd7a38dabf3eb33f13e1816076e9cc03543
unknown <qwe6293@nate.com>
2021-09-02 23:38:15 +0900

    메모이제이션 - 미해결

--- section08/section08_11.js
+++ section08/section08_11.js
@@ -1,10 +1,6 @@
 function solution(n){         
     let answer;
-    function DFS(n){
-        if(n===1) return 1;
-        else return n*DFS(n-1); 
-    }
-    answer=DFS(n);
+    
     return answer;
 }
 
