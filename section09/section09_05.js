707397a56f9ff262130f70052f499a5ddd38304a
unknown <qwe6293@nate.com>
2021-10-17 10:45:10 +0900

    송아지 찾기(BFS: 상태트리탐색)

--- section09/section09_05.js
+++ section09/section09_05.js
@@ -1,22 +1,26 @@
 function solution(s, e){  
     let answer=0;
-    let ch=Array.from({length:10001}, ()=>0);
-    let dis=Array.from({length:10001}, ()=>0);
-    let queue=[];
+    let ch = Array.from({length: 10001}, () => 0);
+    let dis = Array.from({length: 10001}, () => 0);
+    let queue = [];
     queue.push(s);
-    ch[s]=1;
-    dis[s]=0;
-    while(queue.length){
-        let x=queue.shift();
-        for(let nx of [x-1, x+1, x+5]){
-            if(nx===e) return dis[x]+1;
-            if(nx>0 && nx<=10000 && ch[nx]===0){
-                ch[nx]=1;
+    ch[s] = 1;
+    dis[s] = 0;
+    
+    console.log('1', answer);
+
+    while (queue.length) {
+        let v = queue.shift();
+        for (let nx of [v+1, v-1, v+5]) {
+            if (nx === e) return dis[v] + 1;
+            if (nx > 0 && nx <= 10000 && ch[nx] === 0) {
+                ch[nx] = 1;
                 queue.push(nx);
-                dis[nx]=dis[x]+1;
+                dis[nx] = dis[v] + 1;
             }
         }
     }
+    console.log('2', answer);
     return answer;
 }
 
