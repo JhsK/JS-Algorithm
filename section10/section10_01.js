0de5f1054d0beb06d7e8f18fa24a6072ad4692f7
unknown <qwe6293@nate.com>
2021-10-21 09:17:44 +0900

    계단오르기

--- section10/section10_01.js
+++ section10/section10_01.js
@@ -1,11 +1,10 @@
 function solution(n){  
     let answer=0;
     let tmp = Array.from({length: n+1}, () => 0);
-
     tmp[1] = 1;
     tmp[2] = 2;
 
-    for(let i = 3; i <= n; i++) {
+    for (let i = 3; i <= n; i++) {
         tmp[i] = tmp[i-2] + tmp[i-1];
     }
     answer = tmp[n];
