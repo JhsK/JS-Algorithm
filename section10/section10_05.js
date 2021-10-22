5678a78db4174dcfb61713ce1b8ab063c3ced846
unknown <qwe6293@nate.com>
2021-10-22 23:14:21 +0900

    최대점수 구하기

--- section10/section10_05.js
+++ section10/section10_05.js
@@ -1,15 +1,14 @@
 function solution(m, arr){  
     let answer=0;
-    let tmp = Array.from({length: m+1}, () => 0);
-
-    for (let i = 0; i < arr.length; i++) {
-        let ps = arr[i][0];
-        let pt = arr[i][1];
-        for (let j = m; j >= pt; j--) {
-            tmp[j] = Math.max(tmp[j], tmp[j-pt]+ps);
+    let dy=Array.from({length:m+1}, ()=>0);
+    for(let i=0; i<arr.length; i++){
+        let ps=arr[i][0];
+        let pt=arr[i][1];
+        for(let j=m; j>=pt; j--){
+            dy[j]=Math.max(dy[j], dy[j-pt]+ps);
         }
     }
-    answer = tmp[m];
+    answer=dy[m];
     return answer;
 }
 
