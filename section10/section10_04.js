05a5e42b62b7ebde8922d2b58a175f941217908c
unknown <qwe6293@nate.com>
2021-10-22 23:04:52 +0900

    동전교환(냅색 알고리즘)

--- section10/section10_04.js
+++ section10/section10_04.js
@@ -3,9 +3,9 @@ function solution(m, coin){
     let tmp = Array.from({length: m+1}, () => 1000);
     tmp[0] = 0;
 
-    for (let i = 0; i < coin.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         for (let j = coin[i]; j <= m; j++) {
-            tmp[j] = Math.min(tmp[j], tmp[j-coin[i]]+1)
+            tmp[j] = Math.min(tmp[j-coin[i]] + 1, tmp[j]);
         }
     }
     answer = tmp[m];
