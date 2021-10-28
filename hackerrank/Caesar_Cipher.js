38ed045d98adc908eb160a6079bfc15ada772f06
JhsK <69228184+JhsK@users.noreply.github.com>
2021-10-28 22:07:35 +0900

    Caesar Cipher

--- hackerrank/Caesar_Cipher.js
+++ hackerrank/Caesar_Cipher.js
@@ -1,15 +1,16 @@
-function rot13(str) {
-    let decodeStr = ""
-    for (let i = 0; i < str.length; i++) {
-        let lettersCode = (str[i].charCodeAt());
-        if (lettersCode >= 65 && lettersCode <= 77) {
-            decodeStr += String.fromCharCode(lettersCode + 13);
-        } else if (lettersCode >= 78 && lettersCode <= 90) {
-            decodeStr += String.fromCharCode(lettersCode - 13);
-        } else {
-            decodeStr += str[i];
-        }
+function caesarCipher(s, k) {
+    let answer = "";
+    let sum = k % 26;
+    for (let i = 0; i < s.length; i++) {
+        let tmp = s[i].charCodeAt() + sum;
+        if (s[i].charCodeAt() >= 97 && s[i].charCodeAt() <= 122) {
+            if (tmp > 122) tmp = (tmp - 122) + 96;
+            answer += String.fromCharCode(String(tmp));
+        } else if (s[i].charCodeAt() >= 65 && s[i].charCodeAt() <= 90) {
+            if (tmp > 90) tmp = (tmp - 90) + 64;
+            answer += String.fromCharCode(String(tmp));
+        } else answer += s[i];
     }
-    return decodeStr;
+    return answer;
+
 }
-console.log(rot13("SERR PBQR PNZC"));
