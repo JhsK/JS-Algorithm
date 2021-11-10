31860328eea10e9fcc6ece32615a003ab378bd3e
JhsK <qwe6293@nate.com>
2021-11-10 23:11:32 +0900

    The Love Letter Mystery - 미해결

---
+++ hackerrank/The_Love_Letter_Mystery.js
@@ -0,0 +1,30 @@
+function calChar(front, back) {
+  let cnt = 0;
+  for (let i = 0; i < front.length; i++) {
+    if (front[i] !== back[i]) {
+      cnt += Math.abs(front[i].charCodeAt() - back[i].charCodeAt());
+    }
+  }
+
+  return cnt;
+}
+
+function theLoveLetterMystery(s) {
+  let answer = 0;
+  if (s !== s.split("").reverse().join("")) {
+    let tmp = Math.floor(s.length / 2);
+    if (s.length % 2 === 0) {
+      let front = s.slice(0, tmp);
+      let back = s.slice(tmp);
+      answer = calChar(front, back);
+    } else {
+      let front = s.slice(0, tmp);
+      let back = s.slice(tmp + 1);
+      answer = calChar(front, back);
+    }
+  }
+
+  return answer;
+}
+
+console.log(theLoveLetterMystery("bafhaef"));
