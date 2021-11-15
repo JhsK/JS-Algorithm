3de7e6d8d58603366e08416d161c91942e7afbd1
JhsK <qwe6293@nate.com>
2021-11-15 22:30:10 +0900

    two string - not solve

---
+++ hackerrank/Two_String.js
@@ -0,0 +1,9 @@
+function twoStrings(s1, s2) {
+  let result = "NO";
+  const s1len = s1.length;
+  const s2len = s2.length;
+  if (s1len >= 1 && s1len <= 100000 && s2len >= 1 && s2len <= 100000) {
+    // continue with code
+  }
+  return result;
+}
