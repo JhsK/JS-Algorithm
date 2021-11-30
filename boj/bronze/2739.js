d1887a044f10a64b516bc1d77a739d4b02f8d989
JhsK <69228184+JhsK@users.noreply.github.com>
2021-11-30 23:14:23 +0900

    2739 - 구구단

--- boj/bronze/2739.js
+++ boj/bronze/2739.js
@@ -1,6 +1,6 @@
 let input = require('fs').readFileSync('/dev/stdin').toString();
 
-let num = Number(input[0]);
+let num = input[0].split(" ");
 
 for (let i = 1; i < 10; i++) {
     console.log(`${num} * ${i} = ${num * i}`);
