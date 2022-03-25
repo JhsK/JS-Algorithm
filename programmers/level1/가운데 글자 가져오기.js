89f70d61c4774bc9f72b14668be5ad660262761c
JhsK <qwe6293@nate.com>
2022-03-25 11:13:24 +0900

    22-03-25

---
+++ programmers/level1/가운데 글자 가져오기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  return s.length % 2 === 0
+    ? s.slice(s.length / 2 - 1, s.length / 2 + 1)
+    : s[Math.floor(s.length / 2)];
+}
+
+function solution(s) {
+  return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
+}
