79ffd7b4c57f9d60f9eb53b218c6642fd52d782d
JhsK <qwe6293@nate.com>
2022-04-17 22:10:10 +0900

    22-04-17

--- programmers/level2/N개의 최소공배수.js
+++ programmers/level2/N개의 최소공배수.js
@@ -29,3 +29,25 @@ function solution(arr) {
 console.log(solution([2, 6, 8, 14]));
 
 // 최소공배수를 구하기 위해 유클리드 호제법을 이용하여 최소공약수를 구하고 그걸 이용하여 최소공배수를 구한다. 여러 개으 최소 공배수는 2개의 수 중 최소공배수를 구하고 구한 최소공배수랑 다음 수랑 다시 최소공배수를 구하는 식으로 문제를 풀어나간다
+
+// const gcd = (a, b) => {
+//   while (b > 0) {
+//     let temp = b;
+//     b = a % b;
+//     a = temp;
+//   }
+//   return a;
+// };
+
+// const lcm = (a, b) => {
+//   return (a * b) / gcd(a, b);
+// };
+
+// const solution = (arr) => {
+//   let answer = 1;
+//   for (let i = 0; i < arr.length; i++) {
+//     answer = lcm(answer, arr[i]);
+//   }
+
+//   return answer;
+// };
