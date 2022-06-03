a0e0739bba812ad533287e821367433c39d8cfb3
JhsK <qwe6293@nate.com>
2022-06-03 10:07:49 +0900

    22-06-03

---
+++ boj/silver/11727.js
@@ -0,0 +1,25 @@
+function solution(input) {
+  let cnt = Number(input);
+  const dp = Array.from({ length: cnt + 1 }, () => 0);
+  dp[1] = 1;
+  dp[2] = 3;
+
+  for (let i = 3; i <= cnt; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1] * 2) % 10007;
+  }
+  console.log(dp[cnt]);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let input;
+rl.on("line", function (line) {
+  input = line;
+  rl.close();
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
