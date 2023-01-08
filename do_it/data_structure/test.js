43cbeb772327c7c36b418e38501734fd1cda0017
JhsK <qwe6293@nate.com>
2023-01-08 23:15:41 +0900

    이진 탐색

--- do_it/data_structure/test.js
+++ do_it/data_structure/test.js
@@ -1,26 +1,36 @@
-let bingo = 0;
+function test(num, arr) {
+  const arrMax = arr[arr.length - 1];
+  const arrSum = arr.reduce((a, b) => a + b);
+  const target = [];
 
-const merge = (left, right) => {
-  const result = [];
-  while (left.length && right.length) {
-    if (left[0] <= right[0]) {
-      bingo++;
+  let start = arrMax;
+  let end = arrSum;
+
+  while (start <= end) {
+    let mid = Math.ceil((start - end) / 2);
+    let sum = 0;
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (sum + arr[i] > mid) {
+        count++;
+        sum = 0;
+      }
+      sum += arr[i];
     }
-    result.push(left[0] <= right[0] ? left.shift() : right.shift());
+
+    if (sum !== 0) {
+      count++;
+    }
+
+    if (count > num) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
   }
-  return [...result, ...left, ...right];
-};
 
-function test(arr) {
-  if (arr.length < 2) return arr;
-
-  let mid = Math.floor(arr.length / 2);
-  let left = arr.slice(0, mid);
-  let right = arr.slice(mid);
-
-  return merge(test(left), test(right));
-
+  return start;
 }
 
-console.log(test([3, 2, 8, 1, 7, 4, 5, 6]));
-console.log(bingo);
+console.log(test(3, [1, 2, 3, 4, 5, 6, 7, 8, 9]));
