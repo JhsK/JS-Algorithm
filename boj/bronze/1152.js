47807ebd65f1360cb665c88f2c5138f78c6579ca
JhsK <qwe6293@nate.com>
2023-09-30 22:54:51 +0900

    이전 백준 문제 기록 제거

--- boj/bronze/1152.js
+++
@@ -1,7 +0,0 @@
-let input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split(" ");
-
-console.log(input[0] === "" ? 0 : input.length);
--- boj/bronze/2739.js
+++
@@ -1,7 +0,0 @@
-let input = require('fs').readFileSync('/dev/stdin').toString();
-
-let num = input[0].split(" ");
-
-for (let i = 1; i < 10; i++) {
-    console.log(`${num} * ${i} = ${num * i}`);
-}
